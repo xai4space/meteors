4072bda8a353b1084931b0e5bfd408b056b221d0
Tymoteusz Kwieciński <31191783+Fersoil@users.noreply.github.com>
2024-09-25 10:55:16 +0200

    docs: fixed docs with reference (#86)
    Co-authored-by: Vladimir Zaigrajew <vladimirzaigrajew@gmail.com>
    Co-authored-by: Vladimir Zaigrajew <41760848+WolodjaZ@users.noreply.github.com>
    Co-authored-by: Fersoil <Fersoil>

--- src/meteors/attr/input_x_gradients.py
+++ src/meteors/attr/input_x_gradients.py
@@ -10,6 +10,15 @@ from meteors.attr import HSIAttributes, Explainer
 
 
 class InputXGradient(Explainer):
+    """
+    Initializes the InputXGradient explainer. The InputXGradients method is a straightforward approach to
+    computing attribution. It simply multiplies the input image with the gradient with respect to the input.
+    This method is based on the [`captum` implementation](https://captum.ai/api/input_x_gradient.html)
+
+    Attributes:
+        _attribution_method (CaptumIntegratedGradients): The InputXGradient method from the `captum` library.
+    """
+
     def __init__(self, explainable_model: ExplainableModel):
         super().__init__(explainable_model)
         self._attribution_method = CaptumInputXGradient(explainable_model.forward_func)
